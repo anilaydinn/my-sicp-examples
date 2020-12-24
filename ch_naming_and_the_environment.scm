1dfa57ba9c5bee447126ca3d2d61b27108698b5e
thracian <anil_aydinn@outlook.com>
2020-12-25 00:20:22 +0300

    naming and the environment chapter examples added.

---
+++ ch_naming_and_the_environment.scm
@@ -0,0 +1,16 @@
+#lang sicp
+
+(define size 2)
+
+(* 5 size)
+
+size
+
+(define pi 3.14159)
+(define radius 10)
+
+(* pi (* radius radius))
+
+(define circumference (* 2 pi radius))
+
+circumference
